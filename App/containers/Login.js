6c910fc6fd48cce0248995ad82db1e62e456730d
danding <1291248710@qq.com>
2017-01-20 22:03:21 +0800

    xx

--- App/containers/Login.js
+++ App/containers/Login.js
@@ -16,11 +16,21 @@ import {
     TextInput,
     TouchableHighlight,
     ActivityIndicator,
-    TabBarIOS
+    TabBarIOS,
+    Dimensions,
+    Button,
+    ScrollView,
+    Alert,
+    TouchableOpacity
 } from 'react-native';
+
+
 
 import { connect } from 'react-redux';
 var t=require('tcomb-form-native');
+var {height, width} = Dimensions.get('window');
+import {BoxShadow} from 'react-native-shadow'
+
 var Form = t.form.Form;
 
 var Person = t.struct({
@@ -31,17 +41,43 @@ var Person = t.struct({
 
 var options = {};
 
+
 import {loginAction} from '../action/actionCreator';
 var Proxy = require('../proxy/Proxy');
 
 
 var  Login =React.createClass({
     onLoginPressed:function () {
-        console.log('attempting to log in with username: ' + this.state.username);
-        this.setState({showProgress: true});
-        const {dispatch} = this.props;
-
-        dispatch(loginAction(this.state.username,this.state.password));
+        var user=this.state.user;
+        var username=user.username;
+        var password=user.password;
+        if(username!==undefined&&username!==null&&username!='')
+        {
+            if(password!==undefined&&password!==null&&password!='')
+            {
+                this.setState({showProgress: true});
+                const {dispatch} = this.props;
+                dispatch(loginAction(username,password));
+            }else{
+                Alert.alert(
+                    '错误',
+                    '请填写密码后再点击登录',
+                    [
+                        {text: 'OK', onPress: () => {
+                        }},
+                    ]
+                );
+            }
+        }else{
+            Alert.alert(
+                '错误',
+                '请填写用户名后再点击登录',
+                [
+                    {text: 'OK', onPress: () => {
+                    }},
+                ]
+            );
+        }
     },
     onPress:function () {
         var form = this.refs.form.getValue();
@@ -51,14 +87,34 @@ var  Login =React.createClass({
         dispatch(loginAction(form.用户名,form.密码));
     },
     getInitialState:function(){
-        return ({showPregress: true});
+        return ({showPregress: true,user:{}});
     },
+
+
     render:function () {
-        var t1=
+
+
+        var t1=(
+            <View>
+            </View>
+
+            );
+
+        var t2=(
             <View style={styles.container}>
-                <Text style={styles.heading}>
-                    Github Browser
-                </Text>
+                <Form
+                    ref="form"
+                    type={Person}
+                    options={options}
+                />
+                <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
+                    <Text style={styles.buttonText}>登录</Text>
+                </TouchableHighlight>
+            </View>
+        );
+
+        var remain=(
+            <View>
                 <Image style={styles.logo} source={require('../img/Octocat.png')} />
 
                 <View style={{justifyContent:'center',alignItems:'center'}}>
@@ -94,17 +150,128 @@ var  Login =React.createClass({
                     style={[styles.loader, {height: 80}]}
                     size="large"
                 />
-            </View>;
-        return (
+            </View>
+        );
+
+
+
+        var t4=(
             <View style={styles.container}>
-                <Form
-                    ref="form"
-                    type={Person}
-                    options={options}
-                />
-                <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
-                    <Text style={styles.buttonText}>登录</Text>
-                </TouchableHighlight>
+                <View style={[{backgroundColor:'#387ef5',padding:10,justifyContent:'center',flexDirection:'row'}]}>
+                    <Text style={{color:'#fff',fontSize:22}}>supnuevo</Text>
+                </View>
+                <View style={{padding:10,flexDirection:'row',justifyContent:'center'}}>
+                    <Image style={styles.logo} source={require('../img/cart.png')} />
+                </View>
+            </View>
+        );
+
+
+        const shadowOpt = {
+            width:width-20,
+            height:200,
+            color:"#000",
+            border:2,
+            radius:3,
+            opacity:0.2,
+            x:0,
+            y:1.5,
+            style:{marginVertical:5}
+        }
+
+        return (
+            <View style={[styles.container]}>
+                <View style={[{backgroundColor:'#387ef5',padding:10,justifyContent:'center',flexDirection:'row'}]}>
+                    <Text style={{color:'#fff',fontSize:22}}>supnuevo</Text>
+                </View>
+
+
+                <View style={{justifyContent:'center',flexDirection:'row',padding:10,marginTop:10}}>
+                    <BoxShadow setting={shadowOpt}>
+                        <View style={{
+                        position:"relative",
+                        width: width-20,
+                        height: 200,
+                        backgroundColor: "#fff",
+                        borderRadius:3,
+                        justifyContent:'center',
+                        flexDirection:'row',
+                        padding:15,
+                        overflow:"hidden"}}>
+                            <Image style={styles.logo} source={require('../img/cart.png')} />
+                        </View>
+                    </BoxShadow>
+                </View>
+
+                <View style={{padding:10,paddingTop:2}}>
+                    {/*输入用户名*/}
+                    <View style={[styles.row,{borderBottomWidth:0}]}>
+
+                        <View style={{flex:1,borderWidth:1,borderColor:'#ddd',flexDirection:'row'}}>
+
+                            <View style={{flex:1,flexDirection:'row',alignItems:'center',justifyContent:'flex-start',padding:16,
+                                paddingLeft:20,paddingRight:15,marginLeft:10}}>
+                                <Text style={{fontSize:16,color:'#444'}}>用户名</Text>
+                            </View>
+
+                            <View style={{flex:5,flexDirection:'row',alignItems:'center'}}>
+                                <TextInput
+                                    style={{height: 46,flex:1,paddingLeft:20,paddingRight:10,paddingTop:2,paddingBottom:2,fontSize:16}}
+                                    onChangeText={(username) => {
+
+                                    this.state.user.username=username;
+                                    this.setState({user:this.state.user});
+                                }}
+                                    value={this.state.user.username}
+                                    placeholder='请输入用户名'
+                                    placeholderTextColor="#aaa"
+                                    underlineColorAndroid="transparent"
+                                />
+                            </View>
+                        </View>
+                    </View>
+
+
+                    {/*输入密码*/}
+                    <View style={[styles.row,{borderBottomWidth:0,borderTopWidth:0}]}>
+
+                        <View style={{flex:1,borderWidth:1,borderColor:'#ddd',flexDirection:'row'}}>
+
+                            <View style={{flex:1,flexDirection:'row',alignItems:'center',justifyContent:'flex-start',padding:16,
+                                    paddingLeft:20,paddingRight:15,marginLeft:10}}>
+                                <Text style={{fontSize:16,color:'#444'}}>密码</Text>
+                            </View>
+
+                            <View style={{flex:5,flexDirection:'row',alignItems:'center'}}>
+                                <TextInput
+                                    style={{height: 46,flex:1,paddingLeft:20,paddingRight:10,paddingTop:2,paddingBottom:2,fontSize:16}}
+                                    onChangeText={(password) => {
+                                    this.state.user.password=password;
+                                    this.setState({user:this.state.user});
+                                }}
+                                    secureTextEntry={true}
+                                    value={this.state.user.password}
+                                    placeholder='请输入密码'
+                                    placeholderTextColor="#aaa"
+                                    underlineColorAndroid="transparent"
+                                />
+                            </View>
+                        </View>
+                    </View>
+
+                    {/*登录*/}
+                    <View style={[styles.row,{borderBottomWidth:0,marginTop:20}]}>
+
+                        <TouchableOpacity style={{flex:1,backgroundColor:'#387ef5',padding:12,borderRadius:6,flexDirection:'row',
+                                justifyContent:'center'}} onPress={()=>{
+                                             this.onLoginPressed()
+                                          }}>
+                            <Text style={{color:'#fff',fontSize:18}}>登录</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+
             </View>
         );
 
@@ -118,14 +285,11 @@ export default connect(
 
 var styles = StyleSheet.create({
     container: {
-        justifyContent: 'center',
-        marginTop: 50,
-        padding: 20,
-        backgroundColor: '#ffffff'
+        flex:1
     },
     logo: {
-        width: 160,
-        height: 200
+        width: width/2,
+        height: 170
     },
     heading: {
         fontSize: 30,
@@ -144,19 +308,11 @@ var styles = StyleSheet.create({
         borderBottomWidth:0
     },
     title: {
-        fontSize: 30,
-        alignSelf: 'center',
-        marginBottom: 30
+        fontSize: 38,
+        backgroundColor: 'transparent'
     },
     button: {
-        height: 36,
-        backgroundColor: '#48BBEC',
-        borderColor: '#48BBEC',
-        borderWidth: 1,
-        borderRadius: 8,
-        marginBottom: 10,
-        alignSelf: 'stretch',
-        justifyContent: 'center'
+        marginRight: 10
     },
     buttonText: {
         fontSize: 18,
@@ -170,5 +326,11 @@ var styles = StyleSheet.create({
         color: 'red',
         paddingTop: 10,
         fontWeight: 'bold'
+    },
+    row:{
+        flexDirection:'row',
+        borderBottomWidth:1,
+        borderBottomColor:'#222'
     }
+
 });
