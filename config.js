5ed7a6af0c8e34b0829b3052ad57e1465f187b05
lwt001no1 <617085130@qq.com>
2017-07-25 08:59:06 +0800

    xx

--- config.js
+++ config.js
@@ -2,7 +2,8 @@
  * Created by danding on 16/12/9.
  */
 module.exports={
-    server:'http://192.168.1.154:8080/supnuevo'
+    server:'http://202.194.14.71'
+    //server:'http://192.168.1.123:8080/supnuevo'
    //server:'http://158.69.137.173:8080/'
 
 }
