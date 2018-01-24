692a92c43eb9ebb3a0d56def1e16150afb9f51f0
李学庆 <lixueqing@lixueqingdeMacBook-Pro.local>
2018-01-24 16:54:12 +0800

    l

--- config.js
+++ config.js
@@ -3,7 +3,8 @@
  */
 module.exports={
 
-    server:'http://192.168.1.121:8080/supnuevo',
+    //server:'http://192.168.1.110:8080/supnuevo',
+    server:'http://192.168.1.128:8080/supnuevo',//打包部署的地址
     //server:'http://142.4.194.0:8080',//打包部署的地址
     //server:'http://192.168.1.180/supnuevo',
     //server:'http://202.194.14.71',
